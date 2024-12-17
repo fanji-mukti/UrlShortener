58533f647442359037b74d21bd69e804027bb033
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2024-12-17 23:17:21 +0800

    add ITimeProvider

---
+++ src/Core/Services/ITimeProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace UrlShortener.Core.Services
+{
+    /// <summary>
+    /// Provides an abstraction for retrieving the current time in milliseconds.
+    /// </summary>
+    public interface ITimeProvider
+    {
+        /// <summary>
+        /// Gets the current time in milliseconds since the Unix epoch.
+        /// </summary>
+        /// <returns>The current time in milliseconds.</returns>
+        long GetCurrentTimeMilliseconds();
+    }
+}
