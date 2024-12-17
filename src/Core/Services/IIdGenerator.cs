916eb47ea767c2f26baf1121b69f691ab22c7a3e
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2024-12-17 23:21:27 +0800

    add IIdGenerator

---
+++ src/Core/Services/IIdGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace UrlShortener.Core.Services
+{
+    /// <summary>
+    /// Provides a contract for generating unique identifiers.
+    /// </summary>
+    public interface IIdGenerator
+    {
+        /// <summary>
+        /// Generates a unique identifier.
+        /// </summary>
+        /// <returns>A unique identifier as a long value.</returns>
+        long GenerateId();
+    }
+}
