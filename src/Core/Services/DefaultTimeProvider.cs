6db12c7404deb5699fffb7e43a427026822fa020
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2024-12-17 23:20:36 +0800

    add DefaultTimeProvider

---
+++ src/Core/Services/DefaultTimeProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace UrlShortener.Core.Services
+{
+    /// <summary>
+    /// Provides the current time in milliseconds using the system clock.
+    /// </summary>
+    public class DefaultTimeProvider : ITimeProvider
+    {
+        /// <summary>
+        /// Gets the current time in milliseconds since the Unix epoch.
+        /// </summary>
+        /// <returns>The current time in milliseconds.</returns>
+        public long GetCurrentTimeMilliseconds()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+    }
+}
