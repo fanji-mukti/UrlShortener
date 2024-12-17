e2dc3a64517cca284b2560cc49c14144e79f9f98
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2024-12-17 23:53:50 +0800

    add SnowflakeIdGeneratorTests

---
+++ src/Core.UnitTests/Services/SnowflakeIdGeneratorTests.cs
@@ -0,0 +1,28 @@
+﻿namespace UrlShortener.Core.UnitTests.Services
+{
+    public class SnowflakeIdGeneratorTests
+    {
+        private readonly SnowflakeIdGeneratorSteps _steps;
+
+        public SnowflakeIdGeneratorTests()
+        {
+            _steps = new SnowflakeIdGeneratorSteps();
+        }
+
+        [Fact]
+        public void GenerateId_ShouldReturnUniqueId()
+        {
+            _steps.Given_A_TimeProvider_And_SnowflakeIdGenerator();
+            _steps.When_Generating_An_Id();
+            _steps.Then_The_Id_Should_Be_Unique();
+        }
+
+        [Fact]
+        public void GenerateId_ShouldContainTimeBasedComponent()
+        {
+            _steps.Given_A_TimeProvider_And_SnowflakeIdGenerator();
+            _steps.When_Generating_An_Id();
+            _steps.Then_The_Id_Should_Be_TimeBased();
+        }
+    }
+}
