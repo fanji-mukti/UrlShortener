fb012cd7b29f9939aa2962230ed4f42d55f63754
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2024-12-20 22:22:00 +0800

    Add timebase test cases

--- src/Core.UnitTests/Services/SnowflakeIdGeneratorTests.cs
+++ src/Core.UnitTests/Services/SnowflakeIdGeneratorTests.cs
@@ -80,6 +80,90 @@
                     Description = "Generating the ninth unique ID on January 1, 2025, at Data Center 1 with Worker 2"
                 }
             },
+            new object[]
+            {
+                new IdUniquenessTestData
+                {
+                    DataCenterId = 1,
+                    WorkerId = 1,
+                    GeneratedAt = new DateTime(2025, 1, 1, 0, 0, 1, DateTimeKind.Utc),
+                    GeneratedIdCount = 1,
+                    Expectation = 4194439169L,
+                    Description = "Generating the ninth unique ID on January 1, 2025 on different millisecond, at Data Center 1 with Worker 1"
+                }
+            },
+            new object[]
+            {
+                new IdUniquenessTestData
+                {
+                    DataCenterId = 1,
+                    WorkerId = 1,
+                    GeneratedAt = new DateTime(2025, 1, 1, 0, 1, 0, DateTimeKind.Utc),
+                    GeneratedIdCount = 1,
+                    Expectation = 251658375169L,
+                    Description = "Generating the ninth unique ID on January 1, 2025 on different second, at Data Center 1 with Worker 1"
+                }
+            },
+            new object[]
+            {
+                new IdUniquenessTestData
+                {
+                    DataCenterId = 1,
+                    WorkerId = 1,
+                    GeneratedAt = new DateTime(2025, 1, 1, 1, 0, 0, DateTimeKind.Utc),
+                    GeneratedIdCount = 1,
+                    Expectation = 15099494535169L,
+                    Description = "Generating the ninth unique ID on January 1, 2025 on different minute, at Data Center 1 with Worker 1"
+                }
+            },
+            new object[]
+            {
+                new IdUniquenessTestData
+                {
+                    DataCenterId = 1,
+                    WorkerId = 1,
+                    GeneratedAt = new DateTime(2025, 1, 2, 0, 0, 0, DateTimeKind.Utc),
+                    GeneratedIdCount = 1,
+                    Expectation = 362387865735169L,
+                    Description = "Generating the ninth unique ID on January 1, 2025 on different hour, at Data Center 1 with Worker 1"
+                }
+            },
+            new object[]
+            {
+                new IdUniquenessTestData
+                {
+                    DataCenterId = 1,
+                    WorkerId = 1,
+                    GeneratedAt = new DateTime(2025, 1, 2, 0, 0, 0, DateTimeKind.Utc),
+                    GeneratedIdCount = 1,
+                    Expectation = 362387865735169L,
+                    Description = "Generating the ninth unique ID on January 2, 2025 on different hour, at Data Center 1 with Worker 1"
+                }
+            },
+            new object[]
+            {
+                new IdUniquenessTestData
+                {
+                    DataCenterId = 1,
+                    WorkerId = 1,
+                    GeneratedAt = new DateTime(2025, 2, 1, 0, 0, 0, DateTimeKind.Utc),
+                    GeneratedIdCount = 1,
+                    Expectation = 11234023833735169L,
+                    Description = "Generating the ninth unique ID on February 1, 2025 on different hour, at Data Center 1 with Worker 1"
+                }
+            },
+            new object[]
+            {
+                new IdUniquenessTestData
+                {
+                    DataCenterId = 1,
+                    WorkerId = 1,
+                    GeneratedAt = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+                    GeneratedIdCount = 1,
+                    Expectation = 132271570944135169L,
+                    Description = "Generating the ninth unique ID on Januar 1, 2026 on different hour, at Data Center 1 with Worker 1"
+                }
+            },
         };
 
         [Theory]
