ff4f31d2698934d7a7e1a86e5edcc9b50eadb48b
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2024-12-23 22:10:49 +0800

    add IEncoder

---
+++ src/Core/Utilities/IEncoder.cs
@@ -0,0 +1,15 @@
+﻿namespace UrlShortener.Core.Utilities
+{
+    /// <summary>
+    /// Provides an interface for encoding numbers.
+    /// </summary>
+    public interface IEncoder
+    {
+        /// <summary>
+        /// Encodes the specified number into a string.
+        /// </summary>
+        /// <param name="number">The number to encode.</param>
+        /// <returns>A string representation of the encoded number.</return
+        string Encode(long number);
+    }
+}
