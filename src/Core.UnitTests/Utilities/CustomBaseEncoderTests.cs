c5a906c28e285187f5283691ff4f71b54f3d00da
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2024-12-23 22:47:10 +0800

    update encoder test

--- src/Core.UnitTests/Utilities/CustomBaseEncoderTests.cs
+++ src/Core.UnitTests/Utilities/CustomBaseEncoderTests.cs
@@ -1,73 +1,49 @@
 namespace UrlShortener.Core.UnitTests.Utilities
 {
-    using System;
-    using FluentAssertions;
     using Xunit;
-    using UrlShortener.Core.Utilities;
 
     public class CustomBaseEncoderTests
     {
-        private readonly CustomBaseEncoder _encoder;
-
-        public CustomBaseEncoderTests()
-        {
-            _encoder = new CustomBaseEncoder();
-        }
+        private readonly CustomBaseEncoderSteps _steps = new CustomBaseEncoderSteps();
 
         [Fact]
         public void Encode_ShouldReturnBase64EncodedString_WhenNumberIsPositive()
         {
-            // Given
-            long number = 123456789;
-
-            // When
-            string result = _encoder.Encode(number);
-
-            // Then
-            result.Should().NotBeNullOrEmpty();
-            result.Should().Be("8M0kX");
+            _steps
+                .GivenAnEncoder()
+                .GivenANumber(123456789)
+                .WhenEncodingTheNumber()
+                .ThenTheResultShouldBe("7KwoJ");
         }
 
         [Fact]
         public void Encode_ShouldReturnZeroCharacter_WhenNumberIsZero()
         {
-            // Given
-            long number = 0;
-
-            // When
-            string result = _encoder.Encode(number);
-
-            // Then
-            result.Should().NotBeNullOrEmpty();
-            result.Should().Be("0");
+            _steps
+                .GivenAnEncoder()
+                .GivenANumber(0)
+                .WhenEncodingTheNumber()
+                .ThenTheResultShouldBe("0");
         }
 
         [Fact]
         public void Encode_ShouldReturnZeroCharacter_WhenNumberIsNegative()
         {
-            // Given
-            long number = -1;
-
-            // When
-            string result = _encoder.Encode(number);
-
-            // Then
-            result.Should().NotBeNullOrEmpty();
-            result.Should().Be("0");
+            _steps
+                .GivenAnEncoder()
+                .GivenANumber(-1)
+                .WhenEncodingTheNumber()
+                .ThenTheResultShouldBe("0");
         }
 
         [Fact]
         public void Encode_ShouldReturnCorrectBase64EncodedString_ForLargeNumber()
         {
-            // Given
-            long number = 9223372036854775807; // Max value for long
-
-            // When
-            string result = _encoder.Encode(number);
-
-            // Then
-            result.Should().NotBeNullOrEmpty();
-            result.Should().Be("FfG7_zzzzzz");
+            _steps
+                .GivenAnEncoder()
+                .GivenANumber(11234023833735169L)
+                .WhenEncodingTheNumber()
+                .ThenTheResultShouldBe("IY00V01");
         }
     }
 }
