ccde2a40b7da11f309f5eff2f4c0ef43bcbceed7
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2024-12-25 22:41:58 +0800

    add ShortenedUrl Models

---
+++ src/Core/Models/ShortenedUrl.cs
@@ -0,0 +1,16 @@
+﻿namespace UrlShortener.Core.Models
+{
+    /// <summary>
+    /// Represents a shortened Url.
+    /// </summary>
+    /// <param name="OriginalUrl">The original URL that is being shortened.</param>
+    /// <param name="ShortUrl">The shortened URL.</param>
+    /// <param name="CreatedAt">The date and time when the URL was created.</param>
+    /// <param name="ExpiresAt">The optional expiration date and time for the URL.</param>
+    public record ShortenedUrl(
+        string OriginalUrl,
+        string ShortUrl,
+        DateTime CreatedAt,
+        DateTime? ExpiresAt
+    );
+}
