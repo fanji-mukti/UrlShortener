dc71dab646c05321411cc887d533a69b4cee861e
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2024-12-26 22:35:17 +0800

    Add Document Type

---
+++ src/Core/Repositories/Entities/DocumentType.cs
@@ -0,0 +1,23 @@
+﻿namespace UrlShortener.Core.Repositories.Entities
+{
+    /// <summary>
+    /// Specifies the type of document stored in the database.
+    /// </summary>
+    internal enum DocumentType
+    {
+        /// <summary>
+        /// The document type is unknown.
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// The document represents an original URL.
+        /// </summary>
+        OriginalUrl,
+
+        /// <summary>
+        /// The document represents a shortened URL.
+        /// </summary>
+        ShortenedUrl,
+    }
+}
