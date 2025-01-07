2365177962f04ee9fbd69f72635dce85d8626cf6
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-01-07 22:09:45 +0800

    add ShortenedUrlResponse

---
+++ src/Function/DTOs/ShortenedUrlResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace UrlShortener.Function.DTOs
+{
+    /// <summary>
+    /// Represents the response containing the shortened URL details.
+    /// </summary>
+    public sealed class ShortenedUrlResponse
+    {
+        /// <summary>
+        /// Gets or sets the original URL.
+        /// </summary>
+        public string OriginalUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the shortened URL.
+        /// </summary>
+        public string ShortUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the URL was created.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the shortened URL expires, if any.
+        /// </summary>
+        public DateTime? ExpiresAt { get; set; }
+    }
+}
