9827c9a5e3206b7ad135ce8578f0531cb1c89e7a
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-01-07 22:12:06 +0800

    add ShortenUrlRequest

---
+++ src/Function/DTOs/ShortenUrlRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace UrlShortener.Function.DTOs
+{
+    /// <summary>
+    /// Represents the request to shorten a URL.
+    /// </summary>
+    public sealed class ShortenUrlRequest
+    {
+        /// <summary>
+        /// Gets or sets the original URL.
+        /// </summary>
+        /// <value>The original URL to be shortened.</value>
+        public string OriginalUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the shortened URL expires, if any.
+        /// </summary>
+        /// <value>The expiration date and time of the shortened URL, if applicable.</value>
+        public DateTime? ExpiresAt { get; set; }
+    }
+}
