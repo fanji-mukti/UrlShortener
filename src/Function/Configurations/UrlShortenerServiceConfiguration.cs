08cb641b534d04bd87e5c29cceb5af731e20bfea
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-01-14 21:23:38 +0800

    add UrlShortenerServiceConfiguration

---
+++ src/Function/Configurations/UrlShortenerServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace UrlShortener.Function.Configurations
+{
+    /// <summary>
+    /// Represents the configuration settings for the URL shortener service.
+    /// </summary>
+    public sealed class UrlShortenerServiceConfiguration
+    {
+        /// <summary>
+        /// Gets or sets the data center identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier for the data center.
+        /// </value>
+        public long DataCenterId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the worker identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier for the worker.
+        /// </value>
+        public long WorkerId { get; set; }
+    }
+}
