23b7b2c2a7c3af0fddd021525b5c49f14843b28e
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-01-14 21:20:37 +0800

    register Repository Module

--- src/Function/Program.cs
+++ src/Function/Program.cs
@@ -1,13 +1,25 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using UrlShortener.Function.Configurations;
+using UrlShortener.Function.Modules;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+    })
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        var cosmosDbConfig = Configuration.GetCosmosDbConfiguration();
+
+        builder
+            .RegisterModule(new RepositoryModule(cosmosDbConfig));
     })
     .Build();
 
