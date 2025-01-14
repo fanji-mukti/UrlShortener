3a96ff0f16753beea49327ab53c7b35d6a437a3d
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-01-14 21:07:10 +0800

    update cosmos db config

--- src/Function/Configurations/CosmosDbConfiguration.cs
+++ src/Function/Configurations/CosmosDbConfiguration.cs
@@ -3,12 +3,21 @@
     using System.Net;
     using System.Security;
 
+    /// <summary>
+    /// Represents the configuration settings for connecting to an Azure Cosmos DB instance.
+    /// </summary>
     public sealed class CosmosDbConfiguration : IDisposable
     {
         private SecureString? _secureConnectionString;
         private bool _disposed;
 
-        public required string ConnectionString 
+        /// <summary>
+        /// Gets or sets the connection string for the Cosmos DB instance.
+        /// </summary>
+        /// <value>
+        /// The connection string used to connect to the Cosmos DB instance.
+        /// </value>
+        public required string ConnectionString
         {
             get
             {
@@ -18,11 +27,36 @@
             {
                 _secureConnectionString?.Dispose();
                 _secureConnectionString = new NetworkCredential(string.Empty, new SecureString()).SecurePassword;
-            } 
+            }
         }
 
+        /// <summary>
+        /// Gets or sets the connection mode for the Cosmos DB instance.
+        /// </summary>
+        /// <value>
+        /// The connection mode used to connect to the Cosmos DB instance.
+        /// </value>
         public required string ConnectionMode { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the database in the Cosmos DB instance.
+        /// </summary>
+        /// <value>
+        /// The name of the database in the Cosmos DB instance.
+        /// </value>
+        public required string DatabaseName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the container in the Cosmos DB instance.
+        /// </summary>
+        /// <value>
+        /// The name of the container in the Cosmos DB instance.
+        /// </value>
+        public required string ContainerName { get; set; }
+
+        /// <summary>
+        /// Releases all resources used by the <see cref="CosmosDbConfiguration"/> class.
+        /// </summary>
         public void Dispose()
         {
             if (_disposed)
