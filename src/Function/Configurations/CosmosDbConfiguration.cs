bd732e888e3ddd1588430b6efa283d700e473ec6
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-01-14 20:56:46 +0800

    Add CosmosDbConfiguration

---
+++ src/Function/Configurations/CosmosDbConfiguration.cs
@@ -0,0 +1,37 @@
+﻿namespace UrlShortener.Function.Configurations
+{
+    using System.Net;
+    using System.Security;
+
+    public sealed class CosmosDbConfiguration : IDisposable
+    {
+        private SecureString? _secureConnectionString;
+        private bool _disposed;
+
+        public required string ConnectionString 
+        {
+            get
+            {
+                return _secureConnectionString == null ? string.Empty : new NetworkCredential(string.Empty, _secureConnectionString).Password;
+            }
+            set
+            {
+                _secureConnectionString?.Dispose();
+                _secureConnectionString = new NetworkCredential(string.Empty, new SecureString()).SecurePassword;
+            } 
+        }
+
+        public required string ConnectionMode { get; set; }
+
+        public void Dispose()
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            _secureConnectionString?.Dispose();
+            _disposed = true;
+        }
+    }
+}
