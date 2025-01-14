b0ed2247cd4561b3e1096135e15875943c5d93f0
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-01-14 21:19:16 +0800

    add Configuration

---
+++ src/Function/Configurations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace UrlShortener.Function.Configurations
+{
+    internal static class Configuration
+    {
+        public static string GetValue(string key)
+        {
+            return Environment.GetEnvironmentVariable(key);
+        }
+
+        public static CosmosDbConfiguration GetCosmosDbConfiguration()
+        {
+            return new CosmosDbConfiguration
+            {
+                ConnectionString = GetValue("CosmosDb.ConnectionString"),
+                ConnectionMode = GetValue("CosmosDb.ConnectionMode"),
+                DatabaseName = GetValue("CosmosDb.DatabaseName"),
+                ContainerName = GetValue("CosmosDb.ContainerName"),
+            };
+        }
+    }
+}
