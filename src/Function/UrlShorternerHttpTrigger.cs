c4c94a8f9a595b992c70fd1de05e8de8cf4abaca
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-01-15 20:08:32 +0800

    add xml comment

--- src/Function/UrlShorternerHttpTrigger.cs
+++ src/Function/UrlShorternerHttpTrigger.cs
@@ -10,17 +10,31 @@ namespace Function
     using UrlShortener.Function.DTOs;
     using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
 
+    /// <summary>
+    /// Azure Function HTTP trigger for URL shortening operations.
+    /// </summary>
     public class UrlShorternerHttpTrigger
     {
         private readonly IUrlShortenerService _urlShortenerService;
         private readonly IMapper _mapper;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UrlShorternerHttpTrigger"/> class.
+        /// </summary>
+        /// <param name="urlShortenerService">The URL shortener service.</param>
+        /// <param name="mapper">The AutoMapper instance.</param>
         public UrlShorternerHttpTrigger(IUrlShortenerService urlShortenerService, IMapper mapper)
         {
             _urlShortenerService = EnsureArg.IsNotNull(urlShortenerService, nameof(urlShortenerService));
             _mapper = EnsureArg.IsNotNull(mapper, nameof(mapper));
         }
 
+        /// <summary>
+        /// Handles the HTTP POST request to shorten a URL.
+        /// </summary>
+        /// <param name="req">The HTTP request data.</param>
+        /// <param name="shortenUrlRequest">The request body containing the URL to be shortened.</param>
+        /// <returns>An <see cref="IActionResult"/> containing the shortened URL response.</returns>
         [Function("ShortenUrlV1")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/data/shorten")] HttpRequestData req,
@@ -28,7 +42,7 @@ namespace Function
         {
             if (!TryValidateModel(shortenUrlRequest, out var validationResults))
             {
-                 return new BadRequestObjectResult(validationResults);
+                return new BadRequestObjectResult(validationResults);
             }
 
             var shortenedUrl = await _urlShortenerService
@@ -47,17 +61,23 @@ namespace Function
             }
         }
 
+        /// <summary>
+        /// Handles the HTTP GET request to retrieve the original URL from a shortened URL.
+        /// </summary>
+        /// <param name="req">The HTTP request data.</param>
+        /// <param name="shortUrl">The shortened URL.</param>
+        /// <returns>An <see cref="IActionResult"/> that redirects to the original URL or a 404 Not Found result.</returns>
         [Function("shortUrlV1")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/{shortUrl}")] HttpRequestData req,
             [FromRoute] string shortUrl)
-        { 
+        {
             var shortenedUrl = await _urlShortenerService
                 .GetOriginalUrlAsync(shortUrl)
                 .ConfigureAwait(false);
 
             if (shortenedUrl == null)
-            { 
+            {
                 return new NotFoundResult();
             }
 
