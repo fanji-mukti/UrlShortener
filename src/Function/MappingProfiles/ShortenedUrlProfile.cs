4cdd52164d5b5364536274d7d2fee7a880925c9e
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-01-15 20:06:40 +0800

    add ShortenedUrlProfile

---
+++ src/Function/MappingProfiles/ShortenedUrlProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace UrlShortener.Function.MappingProfiles
+{
+    using AutoMapper;
+    using UrlShortener.Core.Models;
+    using UrlShortener.Function.DTOs;
+
+    /// <summary>
+    /// Defines the AutoMapper profile for mapping between <see cref="ShortenedUrl"/> and <see cref="ShortenedUrlResponse"/>.
+    /// </summary>
+    public sealed class ShortenedUrlProfile : Profile
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShortenedUrlProfile"/> class.
+        /// </summary>
+        public ShortenedUrlProfile()
+        {
+            CreateMap<ShortenedUrl, ShortenedUrlResponse>().ReverseMap();
+        }
+    }
+}
