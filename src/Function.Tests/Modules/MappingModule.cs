d4281824c35ddc566ce4f7fbfd691391b4f17b26
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-07-09 21:34:38 +0800

    add mapping module in test

---
+++ src/Function.Tests/Modules/MappingModule.cs
@@ -0,0 +1,24 @@
+﻿namespace Function.Tests.Modules
+{
+    using Autofac;
+    using AutoMapper;
+    using UrlShortener.Function.MappingProfiles;
+
+    internal sealed class MappingModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder
+                .Register(c =>
+                {
+                    var config = new MapperConfiguration(cfg =>
+                    {
+                        cfg.AddProfile(new ShortenedUrlProfile());
+                    });
+
+                    return config.CreateMapper();
+                })
+                .SingleInstance();
+        }
+    }
+}
