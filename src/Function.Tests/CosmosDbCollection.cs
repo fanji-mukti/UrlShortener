0b308472f59ddc8593c7f1983a6c79a44cfb6f39
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-07-09 21:50:44 +0800

    add UrlShortenerHttpTriggerTests

---
+++ src/Function.Tests/CosmosDbCollection.cs
@@ -0,0 +1,13 @@
+﻿namespace Function.Tests
+{
+    using Xunit;
+
+    [CollectionDefinition(CosmosDbCollection.Name)]
+    public sealed class CosmosDbCollection : ICollectionFixture<TestFixture>
+    {
+        public const string Name = "CosmosDbCollection";
+        // This class is used to apply the TestFixture to all tests in the assembly.
+        // It ensures that the TestFixture is created once per test collection.
+        // No additional code is needed here, as the TestFixture handles the setup and teardown.
+    }
+}
---
+++ src/Function.Tests/UrlShortenerHttpTriggerTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Function.Tests
+{
+    using Autofac;
+    using Xunit;
+
+    [Collection(CosmosDbCollection.Name)]
+    public sealed class UrlShortenerHttpTriggerTests
+    {
+        private readonly UrlShorternerHttpTriggerSteps steps;
+
+        public UrlShortenerHttpTriggerTests(TestFixture fixture)
+        {
+            this.steps = fixture.Container.Resolve<UrlShorternerHttpTriggerSteps>();
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var a = this.steps;
+        }
+    }
+}
