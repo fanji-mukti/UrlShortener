023c8c804c2262432a1a3f963aef979ad3a1d695
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-07-09 21:39:08 +0800

    add FunctionTriggerModule

---
+++ src/Function.Tests/Modules/FunctionTriggerModule.cs
@@ -0,0 +1,17 @@
+﻿namespace Function.Tests.Modules
+{
+    using Autofac;
+    using UrlShortener.Function;
+
+    internal sealed class FunctionTriggerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder
+                .RegisterType<UrlShorternerHttpTrigger>();
+
+            builder
+                .RegisterType<CosmosDbTrigger>();
+        }
+    }
+}
