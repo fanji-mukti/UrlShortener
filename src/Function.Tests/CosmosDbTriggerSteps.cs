87644f95859c5d044f58095bc9441a8fab839895
Fanji Ari Mukti <fanji.arimukti@outlook.com>
2025-07-14 22:33:25 +0800

    add CosmosDbTriggerSteps

---
+++ src/Function.Tests/CosmosDbTriggerSteps.cs
@@ -0,0 +1,35 @@
+﻿namespace Function.Tests
+{
+    using FluentAssertions;
+    using UrlShortener.Core.Models;
+    using UrlShortener.Core.Repositories;
+    using UrlShortener.Function;
+
+    public sealed class CosmosDbTriggerSteps
+    {
+        private readonly CosmosDbTrigger _function;
+        private readonly IUrlRepository _urlRepository;
+        private ShortenedUrl _shortenedUrl;
+
+        public CosmosDbTriggerSteps(CosmosDbTrigger trigger, IUrlRepository urlRepository)
+        {
+            ArgumentNullException.ThrowIfNull(trigger, nameof(trigger));
+            ArgumentNullException.ThrowIfNull(urlRepository, nameof(urlRepository));
+
+            _function = trigger;
+            _urlRepository = urlRepository;
+        }
+
+        public Task WhenICallRunAsync(ShortenedUrl shortenedUrl)
+        { 
+            this._shortenedUrl = shortenedUrl;
+            return _function.RunAsync(new List<ShortenedUrl> { shortenedUrl });
+        }
+
+        public async Task ThenTheUrlShouldBeAddedInTheLookup()
+        {
+            var shortenedUrl = await _urlRepository.GetByOriginalUrlAsync(_shortenedUrl.OriginalUrl).ConfigureAwait(false);
+            shortenedUrl.Should().BeEquivalentTo(_shortenedUrl);
+        }
+    }
+}
--- src/Function.Tests/UrlShortenerHttpTriggerSteps.cs
+++ src/Function.Tests/UrlShortenerHttpTriggerSteps.cs
@@ -4,7 +4,7 @@
     using Microsoft.AspNetCore.Mvc;
     using UrlShortener.Function.DTOs;
 
-    public class UrlShorternerHttpTriggerSteps
+    public sealed class UrlShorternerHttpTriggerSteps
     {
         private readonly UrlShorternerHttpTrigger _function;
         private IActionResult _result;
